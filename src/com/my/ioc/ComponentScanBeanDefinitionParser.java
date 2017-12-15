bb2c5a4db1d32138edd99f066c11e5034a284cf9
reverence <reverence_cz@163.com>
2017-12-15 15:56:06 +0800

    Update ComponentScanBeanDefinitionParser.java

--- src/com/my/ioc/ComponentScanBeanDefinitionParser.java
+++ src/com/my/ioc/ComponentScanBeanDefinitionParser.java
@@ -5,6 +5,8 @@ import org.dom4j.Element;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,16 +14,75 @@ import java.util.Map;
  */
 public class ComponentScanBeanDefinitionParser implements BeanDefinitionParser {
     @Override
-    public void parse(Element element, Map<String, BeanDefinition> beanDefinitionMap) throws IOException {
+    public void parse(Element element, Map<String, BeanDefinition> beanDefinitionMap) {
         String basePackage = element.attributeValue("base-package");
         if(null != basePackage && !"".equals(basePackage.trim())){
-            basePackage = basePackage.replace(".","/");
-            URL base = this.getClass().getResource("");
-            String f = new File(base.getFile()).getCanonicalPath();
-            String path = this.getClass().getResource("").toString();
-            File file = new File(basePackage);
-            if(file.exists()){
-                File fa[] = file.listFiles();
+            List<String>classes = readClassesFromPackage(basePackage);
+            for(String s : classes){
+                Class cls = null;
+                try {
+                    cls = Class.forName(s);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if(cls.isAnnotationPresent(MyService.class)){
+                    MyService value = (MyService) cls.getAnnotation(MyService.class);
+                    String name = value.value();
+                    if(null == name || "".equalsIgnoreCase(name.toString())){
+                        String[] str = s.split("\\.");
+                        name = str[str.length-1];
+                        name = name.substring(0,1).toLowerCase()+name.substring(1);
+                        BeanDefinition bean = new BeanDefinition();
+                        bean.setClassName(s);
+                        bean.setId(name);
+                        if(beanDefinitionMap.containsKey(name))
+                            throw new RuntimeException("repeat definition name:"+name);
+                        beanDefinitionMap.put(name,bean);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<String> readClassesFromPackage(String basePackage) {
+        ArrayList<String>classList = new ArrayList<String>();
+        basePackage = basePackage.replace(".","\\");
+        String path = System.getProperty("java.class.path");
+        String[] paths = path.split(";");
+        for(String s : paths){
+            if(!s.endsWith(".jar")){//只有一个目录
+                path = s;
+                break;
+            }
+        }
+        String dirPath = path+"\\"+basePackage;
+        List<String> classes = new ArrayList<String>();
+        getFiles(path,classes);
+        for(String s : classes){
+            s = s.replace(path+"\\","");
+            s = s.replace("\\",".");
+            s = s.replace(".class","");
+            classList.add(s);
+        }
+        return classList;
+    }
+
+    private void getFiles(String path,List<String>classes){
+        File file = new File(path);
+        if(file.exists()){
+            File[] fileList = file.listFiles();
+            if(null != fileList) {
+                for(File f : fileList){
+                    if(f.isDirectory()){
+                        getFiles(f.getAbsolutePath(),classes);
+                    }else{
+                        String fileName = f.getName();
+                        if(fileName.endsWith(".class")){
+                            classes.add(f.toString());
+                        }
+                    }
+                }
             }
         }
     }
